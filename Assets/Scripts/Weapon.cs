e0f909ea5039fd2a8c4d129201f8978d260d2fe4
a <jeepi123456@gmail.com>
2020-01-25 21:32:32 +0200

    Reloading and bullet changes

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -7,25 +7,48 @@ public class Weapon : MonoBehaviour
     public GameObject projectile;
     //public GameObject shotEffect;
     public Transform shotPoint;
-
+    public float clipSize;
+    private float clipSizeReal;
+    public float reloadTime;
+    private float reloadTimeHidden;
     private float timeBtwShots;
     public float startTimeBtwShots;
-
+    private void Start()
+    {
+        clipSizeReal = clipSize;
+        reloadTimeHidden = reloadTime;
+    }
     private void FixedUpdate()
     {
-        if (timeBtwShots <= 0)
+        if(clipSizeReal > 0)
         {
-            if (Input.GetMouseButton(0))
+            if (timeBtwShots <= 0)
             {
-                //Instantiate(shotEffect, shotPoint.position, Quaternion.identity);
-                Instantiate(projectile, shotPoint.position, transform.rotation);
-                timeBtwShots = startTimeBtwShots;
+                if (Input.GetMouseButton(0))
+                {
+                    //Instantiate(shotEffect, shotPoint.position, Quaternion.identity);
+                    Instantiate(projectile, shotPoint.position, transform.rotation);
+                    timeBtwShots = startTimeBtwShots;
+                    clipSizeReal -= 1;
+                }
+            }
+            else 
+            {
+                timeBtwShots -= Time.fixedDeltaTime;
             }
         }
-        else {
-            timeBtwShots -= Time.fixedDeltaTime;
+        else
+        {
+            if(reloadTimeHidden <= 0)
+            {
+                clipSizeReal = clipSize;
+                reloadTimeHidden = reloadTime;
+            }
+            else
+            {
+                reloadTimeHidden -= Time.fixedDeltaTime;
+            }
+            
         }
-
-       
     }
 }
