576bef500a8020c4d0c0c2b9152d81ab56bf4a88
Oliver <difiter23@gmail.com>
2020-01-26 11:59:52 +0200

    Play and quit button implementation

--- Assets/Scripts/MainMenu/MenuScript.cs
+++ Assets/Scripts/MainMenu/MenuScript.cs
@@ -2,9 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
     // Start is called before the first frame update
     void Start()
     {
