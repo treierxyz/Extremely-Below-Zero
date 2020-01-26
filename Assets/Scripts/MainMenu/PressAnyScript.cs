97f3d3764dbe6f6bab16283d82f95cc6c58c7271
Oliver <difiter23@gmail.com>
2020-01-26 11:39:09 +0200

    Improved main menu and better layout

--- Assets/Scripts/MainMenu/PressAnyScript.cs
+++ Assets/Scripts/MainMenu/PressAnyScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PressAnyScript : MonoBehaviour
 {
+    public GameObject pressAny;
+    public GameObject buttons;
+    public GameObject title;
+    private bool pressed;
+    private Animator animatorT;
+    private bool toggle = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animatorT = title.GetComponent<Animator>();
+        buttons.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.anyKey)
+        {
+            pressed = true;
+            pressAny.SetActive(false);
+            buttons.SetActive(true);
+            animatorT.SetTrigger("transition");
+        }
     }
 }
