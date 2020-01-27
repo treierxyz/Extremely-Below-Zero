226e7cb7975c8397622d35cd43f6396fb8cc0201
treier57lol <33978534+treier57lol@users.noreply.github.com>
2020-01-27 15:11:53 +0200

    Slow-Time script

---
+++ Assets/Scripts/SlowTime.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowTime : MonoBehaviour
+{
+	public float slowDownAmount;
+    private float fixedDeltaTime;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        this.fixedDeltaTime = Time.fixedDeltaTime;
+    }
+
+    // Update is called once per frame
+	void Update()
+    {
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			if (Time.timeScale == 1.0f)
+                Time.timeScale = slowDownAmount;
+            else
+                Time.timeScale = 1.0f;
+            Time.fixedDeltaTime = this.fixedDeltaTime * Time.timeScale;
+		}
+    }
+}
