ba8ab115295562ec09b5800ede458511d86e7f79
Oliver <difiter23@gmail.com>
2020-01-27 18:48:51 +0200

    Better sprite flipping
    Still needs work, while looking left BulletSpawner isn't moved when sprite is flipped

--- Assets/Scripts/PlayerManger.cs
+++ Assets/Scripts/PlayerManger.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerManger : MonoBehaviour
 {
-	//TODO Add arm flip, remove turn on key press, use mouse to turn around
     public float speed;
     private float moveInput;
     private Rigidbody2D rb;
