06bb5c8a28cc0a43e0bdb282f2e4d2dd2941dcf9
a <jeepi123456@gmail.com>
2020-02-01 15:01:06 +0200

    Boss stuff

---
+++ Assets/Scripts/Enemy/BossHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        col.gameObject.GetComponent<PlayerManger>().TakeDamage(1);
+    }
+}
