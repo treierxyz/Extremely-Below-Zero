50a4b7e82181933a1dfb0310e171c72a50cf0f8a
a <jeepi123456@gmail.com>
2020-02-01 16:05:44 +0200

    Slow down stronger, boss stuff

--- Assets/Scripts/Enemy/BossHandler.cs
+++ Assets/Scripts/Enemy/BossHandler.cs
@@ -4,14 +4,41 @@ using UnityEngine;
 
 public class BossHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int health;
+    public float timeBtwHits;
+    private float alsotimeBtwHits;
+    private bool Hit;
+    private void Update()
     {
-        
+        if (health <= 0) 
+        {
+            //Instantiate(deathEffect, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+        if (alsotimeBtwHits <= 0)
+        {
+            Hit = true;
+        }
+        else
+        {
+            Hit = false;
+            alsotimeBtwHits -= Time.fixedDeltaTime;
+        }
     }
 
     void OnTriggerStay2D(Collider2D col)
     {
-        col.gameObject.GetComponent<PlayerManger>().TakeDamage(1);
+        if(col.gameObject.CompareTag("Player") && Hit == true)
+        {
+            col.gameObject.GetComponent<PlayerManger>().TakeDamage(1);
+            alsotimeBtwHits = timeBtwHits;
+        }
+    }
+    
+    public void TakeDamage(int damage) 
+    {
+        
+        //Instantiate(explosion, transform.position, Quaternion.identity);
+        health -= damage;
     }
 }
