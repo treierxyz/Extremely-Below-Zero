c7e5e82b9b3b33b26fd1033f782d637e5519172e
a <jeepi123456@gmail.com>
2020-02-01 17:15:46 +0200

    Boss stuff

--- Assets/Scripts/Enemy/BossHandler.cs
+++ Assets/Scripts/Enemy/BossHandler.cs
@@ -8,7 +8,10 @@ public class BossHandler : MonoBehaviour
     public float timeBtwHits;
     private float alsotimeBtwHits;
     private bool Hit;
-    private void Update()
+
+    
+
+    private void FixedUpdate()
     {
         if (health <= 0) 
         {
@@ -41,4 +44,87 @@ public class BossHandler : MonoBehaviour
         //Instantiate(explosion, transform.position, Quaternion.identity);
         health -= damage;
     }
-}
+
+
+    // Combat
+
+
+    private float attackChoice;
+    private bool attackDone;
+    private float timeBtwAttacks = 2f;
+    private float alsoTimeBtwAttacks;
+    public GameObject bossDefault;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        alsoTimeBtwAttacks = timeBtwAttacks;
+        attackDone = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(attackDone == true)
+        {
+            if (alsoTimeBtwAttacks <= 0)
+            {
+                attackChoice = Random.Range(1, 4);
+                if(attackChoice == 1.0)
+                {
+                    Attack1();
+                }
+                if(attackChoice == 2.0)
+                {
+                    Attack2();
+                } 
+                if(attackChoice == 3.0)
+                {
+                    Attack3();
+                } 
+            }
+            else
+            {
+                alsoTimeBtwAttacks -= Time.fixedDeltaTime;
+            }
+        }
+    }
+    private void Attack1()
+    {
+        attackDone = false;
+        
+        if(gameObject.transform.position == bossDefault.gameObject.transform.position)
+        {
+            attackDone = true;
+            Debug.Log("1");
+            alsoTimeBtwAttacks = timeBtwAttacks;
+        }
+    }
+    private void Attack2()
+    {
+        attackDone = false;
+        if(gameObject.transform.position == bossDefault.gameObject.transform.position)
+        {
+            attackDone = true;
+            Debug.Log("2");
+            alsoTimeBtwAttacks = timeBtwAttacks;
+        }
+    }
+    private void Attack3()
+    {
+        attackDone = false;
+        if(gameObject.transform.position == bossDefault.gameObject.transform.position)
+        {
+            attackDone = true;
+            Debug.Log("3");
+            alsoTimeBtwAttacks = timeBtwAttacks;
+        }
+    }
+
+
+
+
+
+
+
+}   
