6563e66dd3cf89fbd2ee271f87f44d308cebb4d1
Oliver <difiter23@gmail.com>
2020-02-04 21:40:30 +0200

    Elevators!

---
+++ Assets/Scripts/StartElevator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartElevator : MonoBehaviour
+{
+    public Animator leftDoorAnimator;
+    public Animator rightDoorAnimator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftDoorAnimator.SetTrigger("finishTrigger");
+        rightDoorAnimator.SetTrigger("finishTrigger");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
